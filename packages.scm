ae204f21473621c8003b43db21aaae4e9558252c
Eric Knauel <knauel@informatik.uni-tuebingen.de>
2006-07-31 11:08:16 +0200

    Import the Scheme48 enum-set implementation
    darcs-hash:20060731090816-782ad-4eddcb8e7224c4b5e048b7c83c0c1672374650fe.gz

--- packages.scm
+++ packages.scm
@@ -1,3 +1,30 @@
+(define-interface enum-sets-interface
+  (export (define-enum-set-type :syntax)
+          enum-set->list
+          enum-set-member?
+          enum-set=?
+          enum-set-union
+          enum-set-intersection
+          enum-set-negation))
+
+(define-interface enum-sets-internal-interface
+  (export enum-set-has-type?
+          enum-set?
+          enum-set-type
+          enum-set->integer
+          integer->enum-set))
+
+(define-structures ((enum-sets enum-sets-interface)
+                    (enum-sets-internal enum-sets-internal-interface))
+  (open scheme define-record-types
+        finite-types
+        bitwise 
+        util
+        signals
+        external-calls)
+  (optimize auto-integrate)
+  (files enum-set))
+
 (define-structure mysql-low (export)
   (open scheme-with-scsh
 	define-record-types
